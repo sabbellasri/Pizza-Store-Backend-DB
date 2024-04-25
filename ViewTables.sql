35331cea0379e3c2e1c60d0c0d861f4252e45e67
sabbellasri <34259114+sabbellasri@users.noreply.github.com>
2024-04-25 16:42:28 -0400

    Add files via upload

---
+++ ViewTables.sql
@@ -0,0 +1,15 @@
+-- Srinivasa Reddy Sabbella
+USE PIZZERIA;
+
+SELECT * FROM customer;
+SELECT * FROM orders;
+SELECT * FROM dinein;
+SELECT * FROM delivery;
+SELECT * FROM pickup;
+SELECT * FROM base;
+SELECT * FROM pizza;
+SELECT * FROM topping;
+SELECT * FROM pizza_has_topping;
+SELECT * FROM discount;
+SELECT * FROM pizza_discount;
+SELECT * FROM order_discount;
